feee529b65d86453ea216fc1484459ab8c01ed92
Lucas Zanferrari <lucas.zanferrari@gmail.com>
2018-02-22 12:39:06 -0300

    correcting git path in master node

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -2,5 +2,5 @@ package linuxacademy.git;
 
 def gitCommit(gitPath) {
   // this will return the latest commit ID
-  return "/bin/git --git-dir=${gitPath} rev-parse HEAD".execute().text
+  return "/usr/bin/git --git-dir=${gitPath} rev-parse HEAD".execute().text
 }
