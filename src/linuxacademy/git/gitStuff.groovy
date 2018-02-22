e55165fb39f61f5f5ddf229ba03f2316f9226e58
Lucas Zanferrari <lucas.zanferrari@gmail.com>
2018-02-22 12:34:56 -0300

    adding ; to package declaration

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,6 +1,6 @@
-package linuxacademy.git
+package linuxacademy.git;
 
-def gitCommit (gitPath) {
+def gitCommit(gitPath) {
   // this will return the latest commit ID
   return "/bin/git --git-dir=${gitPath} rev-parse HEAD".execute().text
 }
